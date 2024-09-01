57a80ecaade5e04a05da0b8a7ffb1f906cb61bb7
moeneuron <m.binsumait@pm.me>
2024-09-01 14:44:25 +0200

    fix(install.sh): add basic error handling while manual setup

--- install.sh
+++ install.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env zsh
 
-# Clone the repository
-git clone https://github.com/MoeBensu/zsh-some-peco.git
+cd ${ZDOTDIR:-$HOME}
 
-# Append the plugin source line to .zshrc
-echo "\n\n# zsh some peco plugin\n" >> ${ZDOTDIR:-$HOME}/.zshrc
-echo "source ${(q-)PWD}/zsh-some-peco/zsh-some-peco.plugin.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+repo_name="zsh-some-peco"
+repo_url="https://github.com/MoeBensu/$repo_name.git"
+
+if git clone --depth=1 "$repo_url" > /dev/null 2>&1; then
+    echo "Successfully cloned $repo_name repository."
+else
+    if [ -d "$repo_name" ]; then
+        echo "Repository already exists. Skipping clone."
+    else
+        echo "Failed to clone $repo_name repository."
+        git clone --depth=1 "$repo_url"
+    fi
+fi
+
+# Append the plugin source line to .zshrc if it doesn't already exist
+if ! grep -q "source ${(q-)PWD}/$repo_name/zsh-some-peco.plugin.zsh" ${ZDOTDIR:-$HOME}/.zshrc; then
+    echo "\n# some peco zsh plugin" >> ${ZDOTDIR:-$HOME}/.zshrc
+    echo "source ${(q-)PWD}/$repo_name/zsh-some-peco.plugin.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+    echo "Added $repo_name plugin to .zshrc"
+else
+    echo "$repo_name plugin already exists in .zshrc"
+    exit 1
+fi
 
 # Reload the shell configuration
 source ${ZDOTDIR:-$HOME}/.zshrc
